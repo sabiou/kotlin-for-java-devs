044a834b81d0054623387e31d38aeaa71ab0c468
Farouk <sfarouk298@gmail.com>
2019-02-22 11:10:57 +0100

    w2 solution

---
+++ src/w2/checking-identifier.kt
@@ -0,0 +1,23 @@
+package w2
+
+// Implement a function that checks whether a string is a valid identifier.
+// A valid identifier is a non-empty string that starts with a letter or underscore and consists of only letters,
+// digits and underscores.
+
+fun isValidIdentifier(string: String): Boolean {
+    fun isValidCharacter(char: Char) = char == '_' ||
+            char in '0'..'9' || char in 'a'..'z' || char in 'A'..'Z'
+
+    for (char in string) {
+        if (!isValidCharacter(char)) return false
+        if (string.isEmpty() || string[0] in '0'..'9') return false
+    }
+    return true
+}
+
+fun main() {
+    println(isValidIdentifier("farouk"))
+    println(isValidIdentifier("_hello"))
+    println(isValidIdentifier("9dq"))
+    println(isValidIdentifier("-check"))
+}
